b51e6a9bf0b76526b71e54b8bfb47fb03cacfe85
Jean-Olivier Menoubé <jo.menoube@gmail.com>
2022-02-27 11:56:03 +0100

    check for tag name

--- xmlreader.py
+++ xmlreader.py
@@ -8,6 +8,8 @@ def trimtag(pattern,tag):
     m = re.search(pattern,tag)
     return tag.replace(m.group(0), '')
 
+def get_tag_name(element):
+    return trimtag(trimpattern,element.tag)
 def get_child(element):
     return [e for e in element]
 
