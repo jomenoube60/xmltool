3ab84f48c1eb2e6ba2efe0b4ac81faab6b0cb7de
Jean-Olivier Menoubé <jo.menoube@gmail.com>
2022-02-27 11:50:18 +0100

    first commit

---
+++ xmlreader.py
@@ -0,0 +1,15 @@
+import xml.etree.ElementTree as ET
+import re
+tree = ET.parse('document.xml')
+root = tree.getroot()
+
+trimpattern = r'{.*}'
+def trimtag(pattern,tag):
+    m = re.search(pattern,tag)
+    return tag.replace(m.group(0), '')
+
+def get_child(element):
+    return [e for e in element]
+
+def getbody():
+    pass
